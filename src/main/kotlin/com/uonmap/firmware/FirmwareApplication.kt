714231737414775a0a24d356945084cffb3b15a8
svyatoslav <svatoslav-p@yandex.ru>
2022-08-02 21:57:50 +0300

    feat(doc): swagger yaml to annotations

--- src/main/kotlin/com/uonmap/firmware/FirmwareApplication.kt
+++ src/main/kotlin/com/uonmap/firmware/FirmwareApplication.kt
@@ -1,13 +1,24 @@
 package com.uonmap.firmware
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Contact
+import io.swagger.v3.oas.annotations.info.Info
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 @ConfigurationPropertiesScan("com.uonmap.firmware.config")
+@OpenAPIDefinition(
+	info = Info(
+		title = "API Firmware Update Server",
+		version = "0.1",
+		contact = Contact(url = "https://smartsafeschool.com", name = "UonMap", email = "info@uonmap.com"))
+)
 class FrwUpdateApplication
 
 fun main(args: Array<String>) {
 	runApplication<FrwUpdateApplication>(*args)
 }
+
+
--- src/main/kotlin/com/uonmap/firmware/controller/EspApi.kt
+++ src/main/kotlin/com/uonmap/firmware/controller/EspApi.kt
@@ -3,6 +3,17 @@ package com.uonmap.firmware.controller
 import com.uonmap.firmware.exeption.*
 import com.uonmap.firmware.service.EspFileSystem
 import com.uonmap.firmware.service.EspFirmware
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.Parameters
+import io.swagger.v3.oas.annotations.enums.ParameterIn
+import io.swagger.v3.oas.annotations.enums.ParameterStyle
+import io.swagger.v3.oas.annotations.headers.Header
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.core.io.Resource
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -16,6 +27,35 @@ class EspApi(
     private val espFw: EspFirmware,
     private val espFs: EspFileSystem
 ) {
+    @Operation(summary = "Get the update file for ESP32",
+        description = "Returns the update file or an explanation why the file did not attach.")
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "Firmware file",
+                headers= [
+                    (Header(name="x-MD5", description ="MD5 sum of found file"))],
+                content = [
+                    (Content(mediaType = "application/json", array = (
+                            ArraySchema(schema = Schema(type = "object", format = "binary")))))]),
+            ApiResponse(responseCode = "400", description = "Invalid request",
+                content = [
+                    (Content(mediaType = "application/json", array = (
+                            ArraySchema(schema = Schema(implementation = ApiError::class)))))]),
+            ApiResponse(responseCode = "404", description = "File Not Found",
+                content = [
+                    (Content(mediaType = "application/json", array = (
+                            ArraySchema(schema = Schema(implementation = ApiError::class)))))])]
+    )
+    @Parameters(
+        value = [
+            Parameter(name = "x-ESP32-mode", example = "spiffs",
+                description = "Type file response spiffs or sketch", `in` = ParameterIn.HEADER),
+            Parameter(name = "x-ESP32-STA-MAC", example = "24:6F:28:DA:7E:B0",
+                description = "MAC address device", `in` = ParameterIn.HEADER),
+            Parameter(name = "x-ESP32-version", example = "0.0.25",
+                description = "Current version firmware or file system ESP32", `in` = ParameterIn.HEADER)
+        ]
+    )
     @GetMapping("/update")
     fun update(
         @RequestHeader headers: Map<String, String>
